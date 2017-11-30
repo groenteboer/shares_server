6a3fe65fe4cd85e2d3265476c24b526a5cf64fa4
Dimitri Couwenberg <couwenberg@gmail.com>
2017-11-30 21:30:50 +1100

    added server and client

--- server.js
+++ server.js
@@ -1,32 +1,29 @@
 var express = require('express'),
-mongoose = require('mongoose'),
-    bodyParser = require('body-parser')
-;
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
 
 var db = mongoose.connect('mongodb://localhost/shareAPI');
 var Share = require('./models/shareModel');
 
 var app = express();
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3001;
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
 
 app.get('/', function (req, res) {
     res.send('hello this is the api');
 });
 
 app.listen(port, function () {
-    console.log('server currently running on ',port);
+    console.log('server currently running on ', port);
 });
 
-
-var shareRouter =express.Router();
+var shareRouter = express.Router();
 
 shareRouter.route('/shares')
-    .post(function(req, res){
+    .post(function (req, res) {
         var share = new Share(req.body);
         console.log(share);
         share.save();
@@ -34,24 +31,22 @@ shareRouter.route('/shares')
     })
     .get(function (req, res) {
         var query = req.query;
-        // Share.find(query,function(err,shares){
-        //     if(err)
-        //         console.log(err);
-        //     else
-        //         res.json(shares);
-        // });
+        Share.find(query, function (err, shares) {
+            if (err)
+                console.log(err);
+            else
+                res.json(shares);
+        });
     });
 
 shareRouter.route('/shares/:shareId')
-    .get(function(req,res){
-        Share.findById(req.params.shareId, function(err,share){
-            if(err)
+    .get(function (req, res) {
+        Share.findById(req.params.shareId, function (err, share) {
+            if (err)
                 res.status(500).send(err);
             else
                 res.json(share);
         });
     });
 
-
-
-app.use('/api',shareRouter)
+app.use('/api', shareRouter);
