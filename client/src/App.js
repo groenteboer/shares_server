9e5f39e34ff7c128f5af89dd6fa9132aaa6b51e6
Dimitri Couwenberg <couwenberg@gmail.com>
2017-12-01 11:37:05 +1100

    displaying list

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,34 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Shares from './components/Shares';
 import axios from 'axios';
 
 class App extends Component {
 
-     constructor(props) {
-             super();
-             this.props = props;
-             this.state={data:[]}
-         }
-
-    componentDidMount(){
-        axios.get('http://localhost:3000/api/shares/').then( (response) => {
-            console.log(response.data);
-                  this.setState({data:response.data});
-         });
+    constructor(props) {
+        super();
+        this.props = props;
+        this.state = {data: []}
     }
-  render() {
-    return (
-      <div className="App">
-        <Shares>
-            {this.state.data}
-        </Shares>
-      </div>
-    );
-  }
+
+    componentDidMount() {
+        axios.get('http://localhost:3000/api/shares/').then((response) => {
+            console.log(response.data);
+            this.setState({data: response.data});
+        });
+    }
+
+    render() {
+
+        console.log('this.state.data=', this.state.data);
+        return (
+            <div className="App">
+                {this.state.data.map(share =>
+                    <p>{share.code}</p>
+                )}
+            </div>
+        );
+    }
 }
 
 export default App;
